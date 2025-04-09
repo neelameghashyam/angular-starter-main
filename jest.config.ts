6a6e10d78914db7222fcc0666ec0e4f624b00f7b
Shyam <neelameghashyam@gmail.com>
2025-04-09 16:10:30 +0530

    with 0 failed jest cases

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,10 @@ import type { Config } from 'jest';
 const jestConfig: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/e2e/', // Ignore Playwright tests so Jest doesn't try to run them
+  ],
 };
 
 export default jestConfig;
