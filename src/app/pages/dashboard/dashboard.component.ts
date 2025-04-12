82f15876a49cfd2e14664f57062febee0e247987
Shyam <neelameghashyam@gmail.com>
2025-04-12 12:39:32 +0530

    Angular Material Customizable Dashboard Phase-2

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,10 +2,12 @@ import { Component, inject } from '@angular/core';
 import { WidgetComponent } from 'src/app/widget/widget/widget.component';
 import { Widget } from 'src/app/models/dashboard';
 import { DashboardService } from 'src/app/service/dashboard.service';
-
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import{MatMenuModule}from'@angular/material/menu'
 @Component({
   selector: 'app-dashboard',
-  imports: [WidgetComponent],
+  imports: [WidgetComponent,MatIcon,MatButtonModule,MatMenuModule],
   providers:[DashboardService],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
--- src/app/service/dashboard.service.ts
+++ src/app/service/dashboard.service.ts
@@ -1,4 +1,4 @@
-import { Component, Injectable, signal } from '@angular/core';
+import { Component, computed, Injectable, signal } from '@angular/core';
 import { Widget } from '../models/dashboard';
 import { SubscribersComponent } from '../pages/dashboard/widgets/subscribers/subscribers.component';
 import { ViewsComponent } from '../pages/dashboard/widgets/views/views.component';
@@ -17,5 +17,58 @@ export class DashboardService {
       content: ViewsComponent, 
       },
     ]);
+
+addedWidgets = signal<Widget[]>([
+  { 
+    id: 1, 
+    label: 'Subscribers', 
+    content: SubscribersComponent, 
+    
+    }, 
+    { 
+      id: 2, 
+      label: 'Views', 
+      content: ViewsComponent, 
+      },
+]); 
+widgetsToAdd = computed(() => { 
+const addedIds = this.addedWidgets().map(w => w.id); 
+return this.widgets().filter(w => !addedIds.includes (w.id));
+})
+
+addWidget (w: Widget) { 
+  this.addedWidgets.set([...this.addedWidgets(), { ...w}]);
+}
+
+updateWidget(id: number, widget: Partial<Widget>) { 
+  const index = this.addedWidgets().findIndex (w => w.id === id); 
+  if (index !== -1){ 
+  const newWidgets = [...this.addedWidgets()]; 
+  newWidgets [index] = { ...newWidgets [index], ...widget }; 
+  this.addedWidgets.set(newWidgets); 
+  }
+}
+
+moveWidgetToRight(id: number) { 
+  const index = this.addedWidgets().findIndex(w => w.id === id); 
+  if (index === this.addedWidgets().length - 1) { 
+  return; 
+  } 
+  const newWidgets= [...this.addedWidgets()]; 
+  [newWidgets [index], newWidgets [index + 1]] = [{ ...newWidgets [index + 1] }, { ...newWidgets [index]}]; 
+  this.addedWidgets.set(newWidgets);
+}
+
+moveWidgetToLeft(id: number) {
+  const index = this.addedWidgets().findIndex(w => w.id === id); 
+  if (index ===0) { 
+  return; 
+  } 
+  const newWidgets= [...this.addedWidgets()]; 
+  [newWidgets [index], newWidgets [index - 1]] = [{ ...newWidgets [index - 1] }, { ...newWidgets [index]}]; 
+  this.addedWidgets.set(newWidgets);
+}
+
+
   constructor() { }
 }
